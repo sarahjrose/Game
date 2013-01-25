0de317f611753a78099b1c68de48386eb6fdcc41
Sarah <sjrose19@gmail.com>
2013-01-25 17:12:30 -0500

    fixes on JS

--- game.js
+++ game.js
@@ -1,27 +1,29 @@
+var randomnumber;
+
 function play() {
-	var randomnumber = Math.floor(Math.random()*101)
+	randomnumber = Math.floor(Math.random()*101)
 	var input = prompt("Pick a Number?");
 	howHot(input);
-	}	
+}	
 
-	function howHot(input) { 
-		if (input == randomnumber){
-			alert(input + " is the Winner!");
-		}
-		else if (input > randomnumber){
-			alert(" too hot!");
-			input = prompt("Pick a Number?");
-			howHot(input);
-		} 
-		else if (input < randomnumber){
-			alert("too cold!");
-			input = prompt("Pick a Number?");
-			howHot(input);
-		}
-		else
-		{
-			alert("bug!");
-		}
+function howHot(input) { 
+	if (input == randomnumber){
+		alert(input + " is the Winner!");
 	}
+	else if (input > randomnumber){
+		alert(" too hot!");
+		input = prompt("Pick a Number?");
+		howHot(input);
+	} 
+	else if (input < randomnumber){
+		alert("too cold!");
+		input = prompt("Pick a Number?");
+		howHot(input);
+	}
+	else
+	{
+		alert("bug!");
+	}
+}
 	
 	
